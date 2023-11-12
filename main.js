9c45361a919ea4ad61d5d16338e35133616766a3
jasper@multiverso.de <jasper@multiverso.de>
2023-11-12 15:04:45 +0100

    test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getCurrentTimeOfDayInKiloseconds() {
+function getCurrentTime() {
   const currentTime = new Date();
   const hours = currentTime.getHours();
   const minutes = currentTime.getMinutes();
@@ -6,22 +6,30 @@ function getCurrentTimeOfDayInKiloseconds() {
   const milliseconds = currentTime.getMilliseconds();
 
   // Calculate the total time in seconds
-  const totalSeconds = (hours * 3600) + (minutes * 60) + seconds + (milliseconds / 1000);
+  const s = (hours * 3600) + (minutes * 60) + seconds + (milliseconds / 1000);
 
   // Convert to kiloseconds
-  const kiloseconds = (totalSeconds * 0.001).toFixed(4);
+  const ks =(s * 0.001)
+  const p = s / (36*24)
 
-  return kiloseconds;
+  return {s, ks, p, t:currentTime.toTimeString()};
 }
 
-function updateKiloseconds() {
-  const kilosecondsElement = document.getElementById("kiloseconds");
-  const kiloseconds = getCurrentTimeOfDayInKiloseconds();
-  kilosecondsElement.textContent = `${kiloseconds} kS`;
+function updateHtml() {
+  const kse = document.getElementById("kiloseconds")
+  const pe = document.getElementById("percent")
+ const te = document.getElementById("time")
+ 
+  const {s,ks,p, t} = getCurrentTime()
+  
+  
+  kse.textContent = `${ks.toFixed(4)} kS`;
+  pe.textContent = `${p.toFixed(4)} %`
+  te.textContent = t
 }
 
 
 window.addEventListener("load", () => {
-  updateKiloseconds()
-  setInterval(updateKiloseconds, 100);
+  updateHtml()
+  setInterval(updateHtml, 100);
 })
